dfb418919784fdfda9a07e8f0ca87b625c767236
17etro <petrpetryk2016@gmail.com>
2020-07-11 15:12:14 +0300

    use styles and BB container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div>  
       <Layout>
-        <p>Test call</p>
+        <BurgerBuilder>
+        </BurgerBuilder>
       </Layout>
     </div>
   );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Aux from '../../hoc/Auxillary/Auxillary';
 
+import classes from './Layout.module.css';
+
 const layout = ( props ) => {
     return (
         <Aux>
             <div>Toolbar, SideDrawer, Backdrop</div>
-            <main>
+            <main className={classes.Content}>
                 {props.children}
             </main>
         </Aux>
