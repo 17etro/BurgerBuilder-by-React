52f5b675062951b7f35fd48a86374e22cdf1d47c
17etro <petrpetryk2016@gmail.com>
2020-07-12 21:14:31 +0300

    set standart burger structure

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './BurgerIngredient.module.css';
 
-const burgerIngredient = ( props ) => {
+class BurgerIngredient extends Component {
+
+    render () {
     let ingredient = null;
 
-    switch ( props.type ) {
+    switch ( this.props.type ) {
         case ('bread-bottom') :
             ingredient = <div className={classes.BreadBottom}></div>;
             break;
@@ -35,5 +38,10 @@ const burgerIngredient = ( props ) => {
     
     return ingredient;
 }
+}
 
-export default burgerIngredient;
+//BurgerIngredient.PropTypes = {
+//    type : PropTypes.string.isRequired,
+//};
+
+export default BurgerIngredient;
