5b67a1f20c70ba3c07b24906f933db7b2f669eee
17etro <petrpetryk2016@gmail.com>
2020-07-14 21:33:06 +0300

    Removing Ingredients Safely

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-import classes from './buildControl.module.css';
+import classes from './BuildControl.module.css';
 
-import buildControls from '../BuildControls';
-
-const buildControl = ( props ) => {
+const buildControl = ( props ) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>Less</button>
-        <button className={classes.More}>More</button>
+        <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
+        <button className={classes.More} onClick={props.added}>More</button>
     </div>
-};
+);
 
 export default buildControl;
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label : 'Salad', type : 'salad'},
+    { label : 'Bacon', type : 'bacon'},
+    { label : 'Meat', type : 'meat'},
+    { label : 'Cheese', type : 'cheese'}
+]
 const buildControls = ( props ) => (
-    <div>
+    <div className={classes.BuildControls}>
+        <p>Current Price : <strong>{props.price.toFixed(2)}</strong></p>
+        {controls.map( ctrl => {
+            return <BuildControl 
+            key={ctrl.label}
+            label={ctrl.label}
+            type={ctrl.type}
+            added={() => props.ingredientAdded(ctrl.type)}
+            removed={() => props.ingredientRemoved(ctrl.type)}
+            disabled={props.disabled[ctrl.type]}/>
+        })}
     </div>
 );
 
