bf6b2bd86e24d07250a267889c4b1b9b3abeefa6
17etro <petrpetryk2016@gmail.com>
2020-07-17 18:42:34 +0300

    upgrading  Responsive Adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -11,7 +11,7 @@ const toolbar = ( props ) => (
         <div className={classes.Logo}>
             <Logo />
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
