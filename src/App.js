8b9be48bc38f3f529d8939fad8233d9ccec5e1c2
17etro <petrpetryk2016@gmail.com>
2020-08-03 20:22:03 +0300

    Building the Checkout Container

--- src/App.js
+++ src/App.js
@@ -2,24 +2,16 @@ import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
-
-  state = {
-    show : true,
-  };
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({show : false});
-    }, 5000)
-  }
 
   render() {
     return (
       <div>  
         <Layout>
-          {this.state.show ? <BurgerBuilder/> : null}
+          <BurgerBuilder/>
+          <Checkout />
         </Layout>
       </div>
     );
---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
+class Checkout extends Component {
+
+    state = {
+        ingredient : {
+            salad : 1,
+            meat : 1,
+            cheese : 1,
+            bacon : 1
+        }
+    }
+    render() {
+        return (
+            <div>
+                <CheckoutSummary ingredient={this.state.ingredient}/>
+            </div>
+        );
+    }
+};
+
+export default Checkout;
