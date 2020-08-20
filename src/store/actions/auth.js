c08e48963eb7ebd2b73f3064ca83f2fb7c2d8219
17etro <petrpetryk2016@gmail.com>
2020-08-20 13:14:50 +0300

    Storing the Token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -7,10 +7,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: token,
+        userId: userId
     };
 };
 
@@ -35,8 +36,7 @@ export const auth = (email, password, isSignUp) => {
         }
         axios.post(url, authData)
             .then(response => {
-                console.log(response);
-                dispatch(authSuccess(response.data));
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
             })
             .catch(err => {
                 console.log(err.message);
---
+++ src/store/reducers/auth.js
@@ -0,0 +1,37 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    token: null,
+    userId: null,
+    error: null,
+    loading: false
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.AUTH_START:
+            return {
+                ...state,
+                error:  null,
+                loading: true
+            };
+        case actionTypes.AUTH_SUCCESS:
+            return {
+                ...state,
+                token: action.idToken,
+                userId: action.userId,
+                error: null,
+                loading: false
+            };
+        case actionTypes.AUTH_FAIL:
+            return {
+                ...state,
+                error: action.error,
+                loading: false
+            }
+        default:
+            return state
+    }
+};
+
+export default reducer;
